50f8829e6a76fa85e0dc0b14a2463abe03febac4
LazarVell <laza992@gmail.com>
2022-02-24 21:25:41 +0100

    Add files via upload

---
+++ src/com/Lakson/Item.java
@@ -0,0 +1,23 @@
+package com.Lakson;
+
+public class Item {
+    protected String name;
+    protected String brand;
+    protected double price;
+
+    public Item(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
---
+++ src/com/Lakson/ShoppingCart.java
@@ -0,0 +1,40 @@
+package com.Lakson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List itemsInCart = new ArrayList();
+    public double total = 0;
+
+    public void addItemToCart(PerishableItems item) {
+        this.itemsInCart.add(item);
+        total = total + item.price;
+    }
+
+    public void addItemToCart(ClothesItem item) {
+        this.itemsInCart.add(item);
+        total = total + item.price;
+    }
+
+    public void addItemToCart(ApplianceItem item) {
+        this.itemsInCart.add(item);
+        total = total + item.price;
+    }
+
+    public List getShoppingCart() {
+        return this.itemsInCart;
+    }
+
+    public double getTotal() {
+        return this.total;
+    }
+
+    public void printItems() {
+        for (int i=0; i<itemsInCart.size(); i++) {
+            System.out.println(itemsInCart.get(i).toString());
+        }
+    }
+}
+
+
